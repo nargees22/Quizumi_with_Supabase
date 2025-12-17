6a0c494c4c569b6fc965fef3f310f359899c8f15
nargees22 <116160251+nargees22@users.noreply.github.com>
2025-12-17 14:50:15 +0530

    feat: Initialize Quizumi project with basic structure
    Sets up the project with necessary dependencies, Vite configuration, and initial HTML structure for the Quizumi application. Includes type definitions and sample AWS questions to begin development.

---
+++ App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+
+import { Header } from './src/components/Header';
+import { Footer } from './src/components/Footer';
+import HomePage from './src/pages/HomePage';
+import CreateQuizPage from './src/pages/CreateQuizPage';
+import JoinQuizPage from './src/pages/JoinQuizPage';
+import LobbyPage from './src/pages/LobbyPage';
+import PlayerLobby from './src/pages/PlayerLobby';
+import QuizHostPage from './src/pages/QuizHostPage';
+import QuizPlayerPage from './src/pages/QuizPlayerPage';
+import LeaderboardPage from './src/pages/LeaderboardPage';
+import PerformanceReportPage from './src/pages/PerformanceReportPage';
+
+const App = () => {
+  return (
+    <HashRouter>
+      <div className="min-h-screen flex flex-col relative">
+        <div className="background-shapes"></div>
+        <Header />
+        <main className="flex-grow flex flex-col relative">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/create" element={<CreateQuizPage />} />
+            <Route path="/join" element={<JoinQuizPage />} />
+            <Route path="/join/:quizId" element={<JoinQuizPage />} />
+            <Route path="/lobby/:quizId" element={<LobbyPage />} />
+            <Route path="/player-lobby/:quizId" element={<PlayerLobby />} />
+            <Route path="/quiz/host/:quizId" element={<QuizHostPage />} />
+            <Route path="/quiz/player/:quizId/:playerId" element={<QuizPlayerPage />} />
+            <Route path="/leaderboard/:quizId" element={<LeaderboardPage />} />
+            <Route path="/report/:quizId" element={<PerformanceReportPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </HashRouter>
+  );
+};
+
+export default App;
